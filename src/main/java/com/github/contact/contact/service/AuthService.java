5e55643c390666fbdbd98a2e4ff7854e166313bb
Agung  Perdananto <agung.perdananto@sirclo.com>
2024-01-23 20:26:44 +0700

    create auth service

---
+++ src/main/java/com/github/contact/contact/service/AuthService.java
@@ -0,0 +1,51 @@
+package com.github.contact.contact.service;
+
+import java.util.Objects;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.github.contact.contact.entity.User;
+import com.github.contact.contact.model.LoginUserRequest;
+import com.github.contact.contact.model.TokenResponse;
+import com.github.contact.contact.repository.UserRepository;
+import com.github.contact.contact.security.BCrypt;
+
+import jakarta.transaction.Transactional;
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    ValidationService validationService;
+
+    @Transactional
+    public TokenResponse login(LoginUserRequest request){
+        
+        validationService.validate(request);
+
+        User user = userRepository.findById(Objects.requireNonNull(request.getUsername())).orElseThrow(()-> new ResponseStatusException(HttpStatus.UNAUTHORIZED, "username or password is invalid."));
+
+        if (BCrypt.checkpw(request.getPassword(), user.getPassword())){
+            // success
+            user.setToken(UUID.randomUUID().toString());
+            user.setTokenExpiredAt(System.currentTimeMillis() + (1000 * 16 * 24 * 30));
+            userRepository.save(user);
+
+            return TokenResponse.builder()
+                    .token(user.getToken())
+                    .expiredAt(user.getTokenExpiredAt())
+                    .build();
+        } else {
+            // failed
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "username or password is invalid");
+        }
+
+    }
+}
--- src/main/java/com/github/contact/contact/service/UserService.java
+++ src/main/java/com/github/contact/contact/service/UserService.java
@@ -1,7 +1,6 @@
 package com.github.contact.contact.service;
 
 import java.util.Objects;
-import java.util.Set;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,10 +13,8 @@ import com.github.contact.contact.model.RegisterUserRequest;
 import com.github.contact.contact.repository.UserRepository;
 import com.github.contact.contact.security.BCrypt;
 
-import jakarta.validation.Validator;
 import jakarta.transaction.Transactional;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.ConstraintViolationException;
+
 
 
 @Service
@@ -27,15 +24,12 @@ public class UserService {
     private UserRepository userRepository;
 
     @Autowired
-    private Validator validator;
+    private ValidationService validationService;
 
     @Transactional
     public void register(RegisterUserRequest request){
 
-        Set<ConstraintViolation<RegisterUserRequest>> constraintViolations =  validator.validate(request);
-        if (constraintViolations.size() != 0){
-            throw new ConstraintViolationException(constraintViolations);
-        }
+        validationService.validate(request);
 
         if (userRepository.existsById(Objects.requireNonNull(request.getUsername()))){
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "username already registered");
