067af81d4dab7b44a75fdc95cfbae781f76e1c53
Agung  Perdananto <agung.perdananto@sirclo.com>
2024-01-23 13:49:36 +0700

    create address model

---
+++ src/main/java/com/github/contact/contact/entity/Address.java
@@ -0,0 +1,37 @@
+package com.github.contact.contact.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name="addresses")
+public class Address {
+
+    @Id
+    private String id;
+
+    private String street;
+
+    private String city;
+
+    private String province;
+
+    private String country;
+
+    @Column(name="postal_code")
+    private String postalCode;
+
+    @ManyToOne
+    @JoinColumn(name="contact_id", referencedColumnName="id")    
+    private Contact contact;
+}
--- src/main/java/com/github/contact/contact/entity/Contact.java
+++ src/main/java/com/github/contact/contact/entity/Contact.java
@@ -1,14 +1,21 @@
 package com.github.contact.contact.entity;
 
+import java.util.List;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name="contacts")
 public class Contact {
@@ -29,4 +36,7 @@ public class Contact {
     @ManyToOne
     @JoinColumn(name="username", referencedColumnName = "username")
     private User user;
+    
+    @OneToMany(mappedBy = "contact")
+    private List<Address> addresses;
 }
