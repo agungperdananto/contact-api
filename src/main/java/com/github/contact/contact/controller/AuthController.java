a258fe17ab9d8ae9ca1ce66573bbc892254145aa
Agung  Perdananto <agung.perdananto@sirclo.com>
2024-02-20 15:35:13 +0700

    create logout api

--- src/main/java/com/github/contact/contact/controller/AuthController.java
+++ src/main/java/com/github/contact/contact/controller/AuthController.java
@@ -2,10 +2,12 @@ package com.github.contact.contact.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.github.contact.contact.entity.User;
 import com.github.contact.contact.model.LoginUserRequest;
 import com.github.contact.contact.model.TokenResponse;
 import com.github.contact.contact.model.WebResponse;
@@ -27,5 +29,14 @@ public class AuthController {
         TokenResponse response = authService.login(request);
 
         return WebResponse.<TokenResponse>builder().data(response).build();
+    }
+
+    @DeleteMapping(
+        path = "/api/auth/logout",
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public WebResponse<String> logout(User user){
+        authService.logout(user);
+        return WebResponse.<String>builder().data("OK").build();
     }
 }
